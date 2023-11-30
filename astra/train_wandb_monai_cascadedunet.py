9534add5fffe68e487c72f9fda7a3bf8b71ccff7
AKA Math <amithjkamath@users.noreply.github.com>
2023-11-30 15:31:21 +0100

    Fixed minor bugs in cascaded version - should now run with no issues with wandb.

--- astra/train_wandb_monai_cascadedunet.py
+++ astra/train_wandb_monai_cascadedunet.py
@@ -1,84 +1,67 @@
-# Copyright (c) MONAI Consortium
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#     http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import numpy as np
 import torch
 import warnings
+import wandb
+from datetime import datetime
 
 from astra.data.dataloader import get_loader
-from monai.networks.nets import BasicUNet
+from astra.model.CascadedUNet import CascadedUNet
+from monai.utils import set_determinism
 
 from pathlib import Path
-import argparse
-from torch.utils.tensorboard import SummaryWriter
 
 import logging
 import os
 import sys
-from datetime import datetime
 import time
-from collections import defaultdict
 
 warnings.filterwarnings("ignore")
 
 
-def trainer(args):
+def trainer(config):
     data_root = "/Users/amithkamath/data/DLDP/ground_truth_small"
-
     logging.basicConfig(stream=sys.stdout, level=logging.INFO)
-    outpath = os.path.join(args.exp_dir, args.exp)
+
+    outpath = os.path.join(config["exp_directory"], config["exp_name"])
     Path(outpath).mkdir(
         parents=True, exist_ok=True
     )  # create output directory to store model checkpoints
-    now = datetime.now()
-    date = now.strftime("%m-%d-%y_%H-%M")
-    writer = SummaryWriter(
-        outpath + "/" + date
-    )  # create a date directory within the output directory for storing train logs
+    os.makedirs(os.path.join(outpath + "/" + config["date"]), exist_ok=True)
+
+    # Set deterministic training for reproducibility
+    set_determinism(seed=config["seed"])
 
     # create train-validation data loaders
-    list_eval_dirs = [
-        os.path.join(data_root, "DLDP_") + str(i).zfill(3)
-        for i in range(61, 81)
-        if i not in [63, 65, 67, 77]  # missing data
-    ]
-
     list_train_dirs = [
         os.path.join(data_root, "DLDP_") + str(i).zfill(3)
         for i in range(1, 61)
         if i != 40
     ]
 
+    list_val_dirs = [
+        os.path.join(data_root, "DLDP_") + str(i).zfill(3)
+        for i in range(61, 81)
+        if i not in [63, 65, 67, 77]  # missing data
+    ]
+
     data_paths = {
         "train": list_train_dirs,
-        "val": list_eval_dirs,
+        "val": list_val_dirs,
     }
 
     train_loader, val_loader = get_loader(
         data_paths,
-        train_bs=2,
-        val_bs=1,
-        train_num_samples_per_epoch=480,
-        val_num_samples_per_epoch=160,
-        num_works=4,
+        train_bs=config["train_batch_size"],
+        val_bs=config["val_batch_size"],
+        train_num_samples_per_epoch=config["train_batch_size"] * len(list_train_dirs),
+        val_num_samples_per_epoch=config["val_batch_size"] * len(list_val_dirs),
+        num_workers=config["num_workers"],
     )
 
     # create the model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = BasicUNet(
-        spatial_dims=3,
-        in_channels=15,
-        out_channels=1,
-        features=[32, 64, 128, 256, 512, 32],
-    ).to(device)
+    model = CascadedUNet(**config["model_params"]).to(device)
+    wandb.watch(model, log_freq=100)
     print("#model_params:", np.sum([len(p.flatten()) for p in model.parameters()]))
 
     # create the loss function
@@ -86,20 +69,22 @@ def trainer(args):
 
     # create the optimizer and the learning rate scheduler
     optimizer = torch.optim.Adam(
-        model.parameters(), lr=args.lr, weight_decay=args.weight_decay
+        model.parameters(),
+        lr=config["learning_rate"],
+        weight_decay=config["weight_decay"],
     )
     scheduler = torch.optim.lr_scheduler.StepLR(
-        optimizer, args.lr_step_size, args.lr_gamma
+        optimizer, config["lr_decay_step_size"], config["lr_gamma"],
     )
 
     # start a typical PyTorch train loop
-    val_interval = 2  # doing validation every 2 epochs
+    val_interval = config["val_interval"]  # doing validation every 2 epochs
     best_metric = 1e10
     best_metric_epoch = -1
     tic = time.time()
-    for epoch in range(args.num_epochs):
+    for epoch in range(config["num_epochs"]):
         print("-" * 10)
-        print(f"epoch {epoch + 1}/{args.num_epochs}")
+        print(f"epoch {epoch + 1}/{config['num_epochs']}")
         model.train()
         epoch_loss = 0
         step = 0
@@ -121,11 +106,12 @@ def trainer(args):
             # Used for counting average loss of this epoch
             optimizer.step()
             epoch_loss += loss.item()
+            wandb.log({"train/loss": loss.item()})
             print(f"{step}, train_loss: {epoch_loss / step:.4f}", "\r", end="")
 
         scheduler.step()
         epoch_loss /= step
-        writer.add_scalar("train_loss", epoch_loss, epoch + 1)
+        wandb.log({"train/loss_epoch": epoch_loss})
         print(
             f"epoch {epoch + 1} average loss: {epoch_loss:.4f} time elapsed: {(time.time()-tic)/60:.2f} mins"
         )
@@ -150,6 +136,7 @@ def trainer(args):
                     val_loss_array.append(val_loss.item())
 
                 metric = np.mean(val_loss_array)
+                wandb.log({"val/metric": metric})
 
                 # save the best checkpoint so far
                 if metric < best_metric:
@@ -157,7 +144,10 @@ def trainer(args):
                     best_metric_epoch = epoch + 1
                     torch.save(
                         model.state_dict(),
-                        os.path.join(outpath + "/" + date, "unet_dose_prediction.pt"),
+                        os.path.join(
+                            outpath + "/" + config["date"],
+                            "cascadedunet_dose_prediction.pt",
+                        ),
                     )
                     print("saved new best metric model")
                 print(
@@ -165,63 +155,58 @@ def trainer(args):
                         epoch + 1, metric, best_metric, best_metric_epoch
                     )
                 )
-                writer.add_scalar("val_mae", metric, epoch + 1)
 
+    wandb.log(
+        {"best_metric": best_metric, "best_metric_epoch": best_metric_epoch,}
+    )
     print(
         f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}"
     )
-    writer.close()
 
 
 def main():
-    parser = argparse.ArgumentParser()
 
-    # train params
-    parser.add_argument(
-        "--num_epochs", default=100, type=int, help="number of train epochs"
+    now = datetime.now()
+    date = now.strftime("%m-%d-%y_%H-%M")
+
+    config = {
+        # experiment settings
+        "exp_directory": "/Users/amithkamath/repo/astra/output",
+        "exp_name": "cascaded-unet-test",
+        "date": date,
+        # data
+        "cache_rate": 1.0,
+        "num_workers": 4,
+        "seed": 1,
+        # train settings
+        "num_epochs": 5,
+        "val_interval": 2,  # check validation score after n epochs
+        "train_batch_size": 2,
+        "val_batch_size": 1,
+        "learning_rate": 1e-4,
+        "lr_scheduler": "stepLR",  # just to keep track
+        "lr_decay_step_size": 10,
+        "lr_gamma": 0.5,
+        "weight_decay": 0.0,
+        # Unet model
+        "model_type": "CascadedUNet",  # just to keep track
+        "model_params": dict(
+            spatial_dims=3,
+            in_channels=15,
+            out_channels=1,
+            channels_first=(16, 32, 64, 128, 256),
+            channels_second=(32, 64, 128, 256, 512),
+            strides=(2, 2, 2, 2),
+            act="ReLU",
+        ),
+    }
+
+    project_name = date + "-seed-" + str(config["seed"])
+    wandb.init(
+        project="CascadedUNet-small-dose-prediction", name=project_name, config=config,
     )
-
-    parser.add_argument(
-        "--exp_dir",
-        default="/Users/amithkamath/repo/astra/output",
-        type=Path,
-        help="output directory to save train logs",
-    )
-
-    parser.add_argument(
-        "--exp",
-        default="monai_testing",
-        type=str,
-        help="experiment name (a folder will be created with this name to store the results)",
-    )
-
-    parser.add_argument("--lr", default=0.001, type=float, help="learning rate")
-
-    parser.add_argument(
-        "--lr_step_size",
-        default=10,
-        type=int,
-        help="decay learning rate every lr_step_size epochs",
-    )
-
-    parser.add_argument(
-        "--lr_gamma",
-        default=0.1,
-        type=float,
-        help="every lr_step_size epochs, decay learning rate by a factor of lr_gamma",
-    )
-
-    parser.add_argument(
-        "--weight_decay", default=0.0, type=float, help="ridge regularization factor"
-    )
-
-    # model specific args
-    parser.add_argument(
-        "--drop_prob", default=0.0, type=float, help="dropout probability for U-Net"
-    )
-
-    args = parser.parse_args()
-    trainer(args)
+    trainer(config)
+    wandb.finish()
 
 
 if __name__ == "__main__":
